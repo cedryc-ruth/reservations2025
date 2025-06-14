e1c03a0d0ebed870a344980aed4ec6809ecf224b
kenzeg <kenza_zeggaf@hotmail.com>
2025-06-14 21:27:10 +0200

    Creation Api

---
+++ app/Http/Controllers/ArtistApiController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Artist;
+
+use Illuminate\Http\Request;
+
+class ArtistApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Artist::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'firstname' => 'required|string|max:60',
+            'lastname' => 'required|string|max:60',
+        ]);
+
+        $artist = Artist::create($validated);
+
+        return response()->json($artist, 201);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $artist = Artist::find($id);
+
+        if (!$artist) {
+            return response()->json(['message' => 'Artist not found'], 404);
+        }
+
+        return response()->json($artist, 200);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $artist = Artist::find($id);
+
+        if (!$artist) {
+            return response()->json(['message' => 'Artist not found'], 404);
+        }
+
+        $validated = $request->validate([
+            'firstname' => 'sometimes|string|max:255',
+            'lastname' => 'sometimes|string|max:255',
+        ]);
+
+        $artist->update($validated);
+
+        return response()->json($artist, 200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $artist = Artist::find($id);
+
+        if (!$artist) {
+            return response()->json(['message' => 'Artist not found'], 404);
+        }
+
+        $artist->delete();
+
+        return response()->json(['message' => 'Artist deleted'], 200);
+
+    }
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -116,6 +116,6 @@ class UserController extends Controller
         return redirect()->route('user.index');
     }
 
-    }
-   
 }
+   
+

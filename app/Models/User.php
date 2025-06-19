1525b8b59d070bb0e6112b423fa360757b545a65
mehdibenabdelouahed <mehdi.benabdelouahed@hotmail.be>
2025-06-19 10:59:35 +0200

    ajour auth basic api + supp token

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,11 +10,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
-use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -8,35 +8,10 @@ use App\Http\Controllers\LocationApiController;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
-Route::post('/login', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (!$user || !Hash::check($request->password, $user->password)) {
-        return response()->json(['message' => 'Invalid credentials'], 401);
-    }
-
-    $token = $user->createToken('api-token')->plainTextToken;
-
-    return response()->json(['token' => $token], 200);
-});
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/tokens/create', function (Request $request) {
-        $token = $request->user()->createToken($request->token_name);
-
-        return ['token' => $token->plainTextToken];
-    });
-
     Route::get('/user', function (Request $request) {
         return $request->user();
-    });
-});
+    })->middleware('auth:sanctum');
 
-Route::apiResource('artists', ArtistApiController::class)->middleware('auth:sanctum');
-Route::apiResource('shows', ShowApiController::class)->middleware('auth:sanctum');
-Route::apiResource('locations', LocationApiController::class)->middleware('auth:sanctum');
+Route::apiResource('artists', ArtistApiController::class)->middleware('auth.basic');
+Route::apiResource('shows', ShowApiController::class)->middleware('auth.basic');
+Route::apiResource('locations', LocationApiController::class)->middleware('auth.basic');
